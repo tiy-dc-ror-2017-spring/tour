012bc887bb58956d01ada13844e13cf94a723347
Russell Osborne <russell@burningpony.com>
2017-03-28 11:51:51 -0400

    Switched Database to pg from sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170327155620) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookings", force: :cascade do |t|
     t.string   "customer_name"
     t.string   "destination"
@@ -29,8 +32,8 @@ ActiveRecord::Schema.define(version: 20170327155620) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["nickname"], name: "index_users_on_nickname", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true, using: :btree
   end
 
 end
